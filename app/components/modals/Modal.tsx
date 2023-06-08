213e32aec95deb2acaaab91096c02d23088a7ed0
Prajwal <prajwalhassan7@gmail.com>
2023-06-08 08:21:18 +0530

    Map Listing Completed

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -172,6 +172,7 @@ const Modal:React.FC<ModalProps> = ({
 
                             {secondaryAction && secondaryActionLabel && (
                                 <Button 
+                                outline
                                 disabled={disabled} 
                                 label={secondaryActionLabel} 
                                 onClick={handleSecondaryAction} 
